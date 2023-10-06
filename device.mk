c04158b506a979ad7f26b7b559fe8cac64f62b77
Leonardo Ledda <leonardoledda@gmail.com>
2023-10-06 20:46:36 +0200

    X00T: Build libprotobuf-cpp-full-3.9.1-vendorcompat
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- device.mk
+++ device.mk
@@ -360,6 +360,11 @@ PRODUCT_COPY_FILES += \
 # PRODUCT_SHIPPING_API_LEVEL indicates the first api level, device has been commercially launched on.
 PRODUCT_SHIPPING_API_LEVEL := 27
 
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full \
+    libprotobuf-cpp-full-3.9.1-vendorcompat
+
 # QCOM
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
@@ -388,10 +393,6 @@ TARGET_COMMON_QTI_COMPONENTS := \
 # QTI Performance
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
-
-# Radio
-PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full
 
 # Recovery
 PRODUCT_PACKAGES += \
